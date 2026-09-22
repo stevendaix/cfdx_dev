43b9a96eb2db446ffae9aeddf67586a948295ace
stevendaix <146651828+stevendaix@users.noreply.github.com>
2026-09-22 13:18:20 +0100

    test(validation): damp SIMPLE pressure correction for pipe case

--- tests/validation/test_vmfl005_poiseuille.cpp
+++ tests/validation/test_vmfl005_poiseuille.cpp
@@ -227,6 +227,7 @@ void check_case(std::size_t nz)
 
     IncompressibleSolverControls c;
     c.algorithm = PressureVelocityAlgorithm::SIMPLE;
+    c.coupling.alpha_p = 0.1;
     c.convergence.max_iterations = 500;
     c.convergence.relative_tolerance = 1e-8;
     c.convergence.continuity_tolerance = 1e-10;
