95a1b205fe0ab42e8d30b2f86fd68f44561e889b
stevendaix <146651828+stevendaix@users.noreply.github.com>
2026-09-22 13:00:11 +0100

    test(numerics): remove obsolete unsupported-scheme assertion

--- tests/unit/test_laplacian.cpp
+++ tests/unit/test_laplacian.cpp
@@ -178,13 +178,7 @@ int main() {
         EXPECT_NEAR(lap(1), -1.0, 1e-12);
     });
 
-    run_case("laplacian_nonorthogonal_schemes_constant_field", []() {
-        Mesh m = make_unit_cube();
-        ScalarCellField f(1, "p", "Pa", 1);
-        f(0) = 42.0;
-        EXPECT_THROW(compute_laplacian(f, m, LaplacianScheme::CORRECTED), std::runtime_error);
-        EXPECT_THROW(compute_laplacian(f, m, LaplacianScheme::LIMITED), std::runtime_error);
-    });
+
 
     return run_all();
 }
