0937300144e0810f94c71ba9979b4811eab18713
stevendaix <146651828+stevendaix@users.noreply.github.com>
2026-09-22 22:33:32 +0100

    test(amg): validate multilevel Poisson convergence and duplicate diagonals

--- tests/unit/test_amg_chebyshev.cpp
+++ tests/unit/test_amg_chebyshev.cpp
@@ -8,6 +8,49 @@
 #include <iostream>
 #include <limits>
 
+
+
+static SparseMatrix make_poisson_1d(std::size_t n) {
+    SparseMatrix A(n, n);
+    for (std::size_t i = 0; i < n; ++i) {
+        A.push_back(i, i, 2.0);
+        if (i > 0) A.push_back(i, i - 1, -1.0);
+        if (i + 1 < n) A.push_back(i, i + 1, -1.0);
+    }
+    A.finalize();
+    return A;
+}
+
+static SparseMatrix make_poisson_2d(std::size_t nx, std::size_t ny) {
+    const std::size_t n = nx * ny;
+    SparseMatrix A(n, n);
+    for (std::size_t y = 0; y < ny; ++y) {
+        for (std::size_t x = 0; x < nx; ++x) {
+            const std::size_t i = y * nx + x;
+            A.push_back(i, i, 4.0);
+            if (x > 0) A.push_back(i, i - 1, -1.0);
+            if (x + 1 < nx) A.push_back(i, i + 1, -1.0);
+            if (y > 0) A.push_back(i, i - nx, -1.0);
+            if (y + 1 < ny) A.push_back(i, i + nx, -1.0);
+        }
+    }
+    A.finalize();
+    return A;
+}
+
+static double true_residual_ratio(const SparseMatrix& A, const Vector& rhs,
+                                  const Vector& correction) {
+    const auto Az = A.matvec(correction);
+    double before2 = 0.0;
+    double after2 = 0.0;
+    for (std::size_t i = 0; i < rhs.size(); ++i) {
+        before2 += rhs(i) * rhs(i);
+        const double ri = rhs(i) - Az[i];
+        after2 += ri * ri;
+    }
+    return std::sqrt(after2 / before2);
+}
+
 int main() {
     using namespace cfdx::core;
 
