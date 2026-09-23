e6ac4aade4e1b021d907b9ca8e9bacec7a61bb37
stevendaix <146651828+stevendaix@users.noreply.github.com>
2026-09-23 15:28:12 +0100

    fix(gui): restore package boundary API import

--- python/cfdx/__init__.py
+++ python/cfdx/__init__.py
@@ -11,7 +11,7 @@ from .setup_model import MeshSelection, Parameter, ParameterType, SetupDiagnosti
 from .validation import ValidationReport, validate_case
 from .mesh_model import MeshCatalog, MeshPatch, read_mesh_catalog
 from .physics_setup import PhysicsSpec, FieldSpec, physics_spec, default_parameters
-from .boundary_setup import BoundaryFieldSpec, BoundaryTypeSpec, scalar_fields_for_boundary_type, boundary_defaults
+from .boundary_setup import BoundaryFieldSpec, BoundaryTypeSpec, fields_for_boundary_type, boundary_defaults
 from .gui import CFDXMainWindow, create_application, launch
 
 __all__ = [
