8d9b0dd68a0b14019f951b7f5e07481b6d0ca844
stevendaix <146651828+stevendaix@users.noreply.github.com>
2026-09-25 19:31:07 +0000

    test(gmres): restore singular saddle-point matrix

--- tests/unit/test_gmres_solver.cpp
+++ tests/unit/test_gmres_solver.cpp
@@ -185,7 +185,7 @@ int main() {
         A.push_back(0, 0, 1.0); A.push_back(0, 2, 1.0);
         A.push_back(1, 1, 1.0); A.push_back(1, 2, 1.0);
         A.push_back(2, 0, -1.0); A.push_back(2, 1, -1.0);
-        A.push_back(2, 2, -2.0); A.push_back(2, 2, -2.0);
+        A.push_back(2, 2, -2.0);
         A.finalize();
 
         Vector b(3);
